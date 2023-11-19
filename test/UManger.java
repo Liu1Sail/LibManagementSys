b9334f8f8cf28a8fdc9b0666b9e028f2d7aef235
刘一帆 <2311889551@qq.com>
2023-11-19 16:10:32 +0800

    实现UserManger用于管理用户登录，注册，注销 在UManager实现了一个演示案例 User是对表Users的reClass 更新README

--- test/UManger.java
+++ test/UManger.java
@@ -6,8 +6,7 @@ import java.sql.SQLException;
 
 public class UManger {
     public static void main(String[] args) throws SQLException {
-        sqlConfig config = sqlConfig.getInstance();
-        config.init("jdbc:mysql://localhost:3306/library" , "root" , "a231398103");
+        sqlConfig.getInstance().init("jdbc:mysql://localhost:3306/library" , "root" , "a231398103");//配置数据库
         UserManager manager = UserManager.getInstance();
         manager.userSignUp("liu" , "123");
         System.out.println(manager.userLogin(1 , "123").getUname());
