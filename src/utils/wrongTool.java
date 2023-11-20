4ddc6032bcc2b52214622797a108f3c4b07b32b1
刘依洋 <tpsv188@126.com>
2023-11-20 13:35:59 +0800

    更改了项目位置

---
+++ src/utils/wrongTool.java
@@ -0,0 +1,21 @@
+package utils;
+
+class pushWrong extends Exception{
+    public pushWrong (Exception message)
+    {
+        super(message);
+    }
+}
+class connectWrong extends Exception{
+    public connectWrong (Exception message)
+    {
+        super(message);
+    }
+}
+class closeWrong extends Exception{
+    public closeWrong(Exception message)
+    {
+        super(message);
+    }
+}
+
