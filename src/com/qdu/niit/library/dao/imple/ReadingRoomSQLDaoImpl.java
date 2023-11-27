6dbbd2b132b68bb284c864a8dc734b0744ca7f46
刘依洋 <tpsv188@126.com>
2023-11-27 14:58:54 +0800

    更改了Dao的返回形势,更改了返回值为void

--- src/com/qdu/niit/library/dao/imple/ReadingRoomSQLDaoImpl.java
+++ src/com/qdu/niit/library/dao/imple/ReadingRoomSQLDaoImpl.java
@@ -9,65 +9,72 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class ReadingRoomSQLDaoImpl extends BaseSQLDaoImpl implements ReadingRoomSQLDao {
-    private static final String CREATE_TABLE_ReadingRoom_SQL = "CREATE TABLE  IF NOT EXISTS Reading_Room(rid INT NOT NULL,uid INT NOT NULL,start_time DATETIME NOT NULL,end_time DATETIME NOT NULL);";
+    private static final String CREATE_TABLE_ReadingRoom_SQL = "CREATE TABLE  IF NOT EXISTS Reading_Room(uid INT NOT NULL,bid INT NOT NULL,start_time DATETIME NOT NULL,end_time DATETIME NOT NULL);";
     private static final String INSERT_ReadingRoom_SQL = "INSERT INTO Reading_Room (uid,bid,start_time,end_time) VALUES(?,?,?,?)";
     private static final String DELETE_ReadingRoomByUid_SQL = "DELETE FROM Reading_Room WHERE uid = ?";
     private static final String DELETE_ReadingRoomByBid_SQL = "DELETE FROM Reading_Room WHERE bid = ?";
     private static final String SELECT_ReadingRoom_SQL = "SELECT * FROM Reading_Room";
     private static final String SELECT_ReadingRoomByUid_SQL = "SELECT * FROM Reading_Room Where uid = ?";
     private static final String SELECT_ReadingRoomByBid_SQL = "SELECT * FROM Reading_Room Where bid = ?";
-    private static final String SELECT_ReadingRoomThanEndTime_SQL = "SELECT * FROM Reading_Room Where EndTime <= ?";
-    private ReadingRoomSQLDaoImpl() throws SQLException {
+    private static final String SELECT_ReadingRoomByEndTime_SQL = "SELECT * FROM Reading_Room Where TO_DAYS(?)<TO_DAYS(NOW());";
+    private ReadingRoomSQLDaoImpl()throws SQLException
+    {
         executeUpdate(CREATE_TABLE_ReadingRoom_SQL);
     }
     private static ReadingRoomSQLDaoImpl instance;
-    /**
-     * @return instance of BorrowingManager
-     */
     public static ReadingRoomSQLDaoImpl getInstance() throws SQLException {
         if(instance == null)
             instance = new ReadingRoomSQLDaoImpl();
         return instance;
     }
     @Override
-    public int insert(ReadingRoom in) throws SQLException {
-        executeUpdate(INSERT_ReadingRoom_SQL,in.getBid(),in.getUid(),String.valueOf(in.getStart_time()),String.valueOf(in.getEnd_time()));
-        return 0;
+    public void insert(ReadingRoom in) throws SQLException {
+        executeUpdate(INSERT_ReadingRoom_SQL,in.getUid(),in.getBid(),in.getEnd_time(),in.getEnd_time());
     }
 
     @Override
-    public int deleteByUid(int uid) throws SQLException {
+    public void deleteByUid(int uid) throws SQLException {
         executeUpdate(DELETE_ReadingRoomByUid_SQL,uid);
-        return 0;
     }
 
     @Override
-    public int deleteByRid(int bid) throws SQLException {
+    public void deleteByRid(int bid) throws SQLException {
         executeUpdate(DELETE_ReadingRoomByBid_SQL,bid);
-        return 0;
     }
 
     @Override
-    public ArrayList<ReadingRoom> getAll() throws SQLException {
-        getMany(SELECT_ReadingRoom_SQL);
-        return null;
+    public ReadingRoom[] getAll() throws SQLException {
+        ArrayList<Object[]> receive = getMany(SELECT_ReadingRoom_SQL);
+        ReadingRoom[] back = new ReadingRoom[receive.size()];
+        for(int i = 0;i<receive.size();i++)
+        {
+            back[i] = new ReadingRoom((int)receive.get(i)[0],(int)receive.get(i)[1],(LocalDateTime) receive.get(i)[2],(LocalDateTime) receive.get(i)[3]);
+        }
+        return back;
     }
 
     @Override
-    public ArrayList<ReadingRoom> getOneByUid(int uid) throws SQLException {
-        getMany(SELECT_ReadingRoomByUid_SQL,uid);
-        return null;
+    public ReadingRoom getOneByUid(int uid) throws SQLException {
+        Object[] receive = getOne(SELECT_ReadingRoomByUid_SQL,uid);
+        ReadingRoom back = new ReadingRoom((int)receive[0],(int)receive[1],(LocalDateTime) receive[2],(LocalDateTime) receive[3]);
+        return back;
     }
 
     @Override
-    public ArrayList<ReadingRoom> getOneByRid(int bid) throws SQLException {
-        executeUpdate(SELECT_ReadingRoomByBid_SQL,bid);
-        return null;
+    public ReadingRoom getOneByRid(int bid) throws SQLException {
+        Object[] receive = getOne(SELECT_ReadingRoomByBid_SQL,bid);
+        ReadingRoom back = new ReadingRoom((int)receive[0],(int)receive[1],(LocalDateTime) receive[2],(LocalDateTime) receive[3]);
+        return back;
     }
 
     @Override
-    public ArrayList<ReadingRoom> getAllByEndTime(LocalDateTime end_time) throws SQLException {
-        executeUpdate(SELECT_ReadingRoomByBid_SQL,String.valueOf(end_time));
-        return null;
+    public ReadingRoom[] getAllByEndTime(LocalDateTime end_time) throws SQLException {
+        ArrayList<Object[]> receive = getMany(SELECT_ReadingRoomByEndTime_SQL,end_time);
+        ReadingRoom[] back = new ReadingRoom[receive.size()];
+        for(int i = 0;i<receive.size();i++)
+        {
+            back[i] = new ReadingRoom((int)receive.get(i)[0],(int)receive.get(i)[1],(LocalDateTime) receive.get(i)[2],(LocalDateTime) receive.get(i)[3]);
+        }
+        return back;
     }
 }
