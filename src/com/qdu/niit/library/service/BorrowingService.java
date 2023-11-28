e7d9fb809339bc544e3f97b82415735d75df8e9b
刘依洋 <tpsv188@126.com>
2023-11-28 13:54:26 +0800

    完善了service的接口并实现,只是个测试例子,到时候不合适就扔掉

---
+++ src/com/qdu/niit/library/service/BorrowingService.java
@@ -0,0 +1,17 @@
+package com.qdu.niit.library.service;
+
+import com.qdu.niit.library.entity.Borrowing;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public interface BorrowingService {
+    boolean ifHaveByUid(int uid)throws SQLException;
+    boolean ifHaveByBid(int bid)throws SQLException;
+    Borrowing[] findAllByEndTime(LocalDateTime end_time)throws SQLException;
+    Borrowing[] findAllByUid(int uid)throws SQLException;
+    Borrowing findOneByBid(int bid)throws SQLException;
+    boolean  insert(Borrowing in)throws SQLException;
+    boolean  deleteByUid(int uid)throws SQLException;
+    boolean  deleteByBid(int bid)throws SQLException;
+}
